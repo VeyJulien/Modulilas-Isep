3e29c03ca3c0ef068212fb9e5746f926b7c46507
mbreviere <maxime.breviere@isep.fr>
2018-06-05 17:58:37 +0200

    BDD avc exemples

--- src/database/MySQLDB.java
+++ src/database/MySQLDB.java
@@ -38,7 +38,7 @@ public class MySQLDB {
 			//Noe co : 	    Conn = DriverManager.getConnection("jdbc:mysql://localhost:8889/Modulilas", "root", "root");
 			//Julien co :   Conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/modulilas", "root", "root");
 			
-			Conn = DriverManager.getConnection("jdbc:mysql://localhost:8889/Modulilas", "root", "root");
+			Conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/modulilas", "root", "");
 			if (Conn != null) {
 				log("Connection Successful! Enjoy. Now it's time to push data");
 			} else {
@@ -159,7 +159,7 @@ public class MySQLDB {
 						Enseignement = "Informatique";
 						break;
 					case "E":
-						Enseignement = "&#233;l&#233;ctronique";
+						Enseignement = "&#201;l&#233;ctronique";
 						break;
 					case "R":
 						Enseignement = "R&#233;seaux";
