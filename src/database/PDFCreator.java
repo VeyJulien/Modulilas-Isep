b1760170bca86ec97687cf01c3ebb8de349637bb
ArthurJRR <arthur.rouaix@isep.fr>
2018-06-12 18:00:04 +0200

    Ajout PDF Creator

---
+++ src/database/PDFCreator.java
@@ -0,0 +1,54 @@
+package database;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+
+public class PDFCreator {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		String pathToProjectFile="C://Users/thinkpad/Code/GitHubRepositories/Modulilas/";
+		
+		String resources = pathToProjectFile+"WebContent/";
+		String html =readHtmlFile(pathToProjectFile+"WebContent/PDFTest.html")+"<img src=\"smile.png\">";//"<h1>Test</h1><p>Hello World LOLILOL</p>";
+		String path=pathToProjectFile+"PDFCreator/";
+		String pdf=String.format("%stest-01.pdf", path);
+		
+		File file = new File(path);
+		file.mkdirs();
+		
+		ConverterProperties properties = new ConverterProperties();
+    	properties.setBaseUri(resources);
+		HtmlConverter.convertToPdf(html, new FileOutputStream(pdf), properties);
+    	//HtmlConverter.convertToPdf(new FileInputStream(path+"test.html"), new FileOutputStream(pdf));
+	}
+	
+	public static String readHtmlFile(String filePath) {
+		String html="";
+		
+		try {
+            Scanner input = new Scanner(System.in);
+
+            File file = new File(filePath);
+
+            input = new Scanner(file);
+
+
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                html+=" "+line;
+            }
+            input.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+		
+		return html;
+	}
+}
